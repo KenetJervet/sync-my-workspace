6f85b4dc64d491808080f5c508853031e044570c
Yukio Usuzumi <anohigisavay@gmail.com>
2016-12-27 01:30:13 +0800

    Works! Hooray!

--- src/SyncMyWorkspace/Versioning.hs
+++ src/SyncMyWorkspace/Versioning.hs
@@ -1,18 +1,30 @@
-{-# LANGUAGE FlexibleInstances      #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE GADTs                  #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE TypeFamilies           #-}
-{-# LANGUAGE UndecidableInstances   #-}
+{-# LANGUAGE AllowAmbiguousTypes       #-}
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE FunctionalDependencies    #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeSynonymInstances      #-}
+{-# LANGUAGE UndecidableInstances      #-}
 
 module SyncMyWorkspace.Versioning where
 
-class Versioned v
+type family Prototype p
 
-data family Prototype :: * -> *
+class (Monad m, Prototype v1 ~ Prototype v2) => AtomicMigration m v1 v2 | v2 -> v1 where
+  atomicMigrate :: v1 -> m v2
 
-class (Monad m, Prototype v1 ~ Prototype v2) => Migration m v1 v2 | v1 -> v2 where
+class (Monad m, Prototype v1 ~ Prototype v2) => Migration m v1 v2 where
   migrate :: v1 -> m v2
 
-instance (Monad m, Migration m v1 v2, Migration m v2 v3) => Migration m v1 v3 where
-  migrate v1 = (migrate v1 :: m v2) >>= migrate
+instance {-# OVERLAPPABLE #-} (Monad m, Migration m v1 v2, AtomicMigration m v2 v3) => Migration m v1 v3 where
+  migrate v1 = do
+    prevV2 <- migrate v1
+    atomicMigrate prevV2
+
+instance {-# OVERLAPPING #-} Monad m => Migration m v1 v1 where
+  migrate = return
+
--- test/SyncMyWorkspace/Tests/Versioning.hs
+++ test/SyncMyWorkspace/Tests/Versioning.hs
@@ -1,24 +1,32 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+
 module SyncMyWorkspace.Tests.Versioning where
 
-import Test.Tasty
-import Test.Tasty.HUnit
-import SyncMyWorkspace.Versioning
+import           SyncMyWorkspace.Versioning
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 data XZero
 
-data XVersion1 = XVersion1
-data XVersion2 = XVersion2
-data XVersion3 = XVersion3
+data XVersion1 = XVersion1 deriving (Show, Eq)
+type instance Prototype XVersion1 = XZero
+data XVersion2 = XVersion2 deriving (Show, Eq)
+type instance Prototype XVersion2 = XZero
+data XVersion3 = XVersion3 deriving (Show, Eq)
+type instance Prototype XVersion3 = XZero
 
-instance Migration IO XVersion1 XVersion2 where
-  migrate XVersion1 = putStrLn "Upgrading to version 2" >> return XVersion2
+instance AtomicMigration IO XVersion1 XVersion2 where
+  atomicMigrate XVersion1 = putStrLn "Migrating to XVersion2" >> return XVersion2
 
-instance Migration IO XVersion2 XVersion3 where
-  migrate XVersion2 = putStrLn "Upgrading to version 3" >> return XVersion3
+instance AtomicMigration IO XVersion2 XVersion3 where
+  atomicMigrate XVersion2 = putStrLn "Migrating to XVersion3" >> return XVersion3
+
 
 tests :: TestTree
 tests = testGroup "tests of versioning"
   [ testCase "" $ do
-      latest <- (migrate XVersion1 :: XVersion3)
+      latest <- migrate XVersion1
       latest @=? XVersion3
   ]
